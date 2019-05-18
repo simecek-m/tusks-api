fcc550d4bb37b182debfc8e0868a35689d67c7f0
Martin Simecek <simecek.m@seznam.cz>
2019-05-18 19:32:39 +0200

    connect MongoDB

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+// imports
 const express = require('express');
+const mongoose = require('mongoose');
 
+// create express app
 const app = express();
 
+// mongodb connection
+mongoose.connect('mongodb://localhost/todo-db', { useNewUrlParser: true}).then(
+	() => console.log('Mongo DB connected successfully!'),
+	error => console.log('Error while connecting mongo database!', error)
+);
+
+
+// run express server on specific port
 app.listen(3000, () => {
 	console.log('server is running...')
 });
