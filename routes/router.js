6b82c1d9c3cd772f8318e40f6ee99b42a92ffbb0
Martin Simecek <simecek.m@seznam.cz>
2019-05-19 13:52:01 +0200

    add comments to /todos routes

--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const Todo = require('../model/todo');
 
+// get all todo lists
 router.get('/todos', function(req, res){
 	Todo.find()
 		.then(data => res.send(data))
@@ -12,11 +13,11 @@ router.get('/todos', function(req, res){
 		});
 });
 
+// create new todo list with default title ('Title') end empty tasks
 router.post('/todos', function(req, res){
 	Todo.create({title: 'Title'})
 		.then(data => res.send(data))
 		.catch(error => console.log(error));
 });
-
 
 module.exports = router;
