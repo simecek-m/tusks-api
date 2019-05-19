115af00377e1b94305e761270517ed2db4515e1c
Martin Simecek <simecek.m@seznam.cz>
2019-05-19 17:06:57 +0200

    more integrate task into todo

--- router/index.js
+++ router/index.js
@@ -4,10 +4,8 @@ const router = express.Router();
 
 // routes
 const todos = require('./routes/todo');
-const tasks = require('./routes/task');
 
 // router middleware
 router.use(todos);
-router.use(tasks);
 
 module.exports = router;
