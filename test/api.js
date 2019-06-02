68695c1eb916df49be8d0083019711faff0002d0
Martin Šimeček <simecek.m@seznam.cz>
2019-06-02 22:54:49 +0200

    add GET /api/todos endpoint test

--- test/api.js
+++ test/api.js
@@ -1,7 +1,31 @@
-describe.skip('/todos', () => {
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const jsonSchema = require('chai-json-schema');
+const app = require('../app.js');
+const should = chai.should();
+
+const taskSchema = require('./schema/task');
+const todoSchema = require('./schema/todo');
+const todosSchema = require('./schema/todos');
+
+chai.use(chaiHttp);
+chai.use(jsonSchema);
+
+describe('/todos', () => {
   it('should GET all todo lists', done => {
-    done();
+    chai.request(app)
+      .get('/api/todos')
+      .end((err, res) => {
+        should.not.exist(err);
+        should.exist(res);
+        res.should.have.status(200);
+        res.body.should.be.jsonSchema(todosSchema);
+        done();
+      });
   });
+});
+
+describe.skip('/todos', () => {
   it('should POST new todo list', done => {
     done(new Error('not implemented yet'));
   });
