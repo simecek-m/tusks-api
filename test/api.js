c5339ee9225cc6bfd5ff0b46fda0b0ed61e1f57d
Martin Šimeček <simecek.m@seznam.cz>
2019-06-02 12:24:56 +0200

    create tests structure

---
+++ test/api.js
@@ -0,0 +1,26 @@
+describe.skip('/todos', () => {
+  it('should GET all todo lists', done => {
+    done();
+  });
+  it('should POST new todo list', done => {
+    done(new Error('not implemented yet'));
+  });
+  it('should UPDATE specific todo list', done => {
+    done(new Error('not implemented yet'));
+  });
+  it('should DELETE specific todo list', done => {
+    done(new Error('not implemented yet'));
+  });
+});
+
+describe.skip('/todos/:id/task', () => {
+  it('should POST new task into todo list', done => {
+    done(new Error('not implemented yet'));
+  });
+  it('should UPDATE specific task in todo list', done => {
+    done(new Error('not implemented yet'));
+  });
+  it('should DELETE specific task in todo list', done => {
+    done(new Error('not implemented yet'));
+  });
+});
