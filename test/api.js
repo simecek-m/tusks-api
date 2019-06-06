200bb4655411d3d7f24bb457c3ae7b0c1309e460
Martin Šimeček <simecek.m@seznam.cz>
2019-06-06 20:50:10 +0200

    seeding database before each test

--- test/api.js
+++ test/api.js
@@ -1,17 +1,39 @@
+const { mongoDb } = require('~root/config');
+const { Seeder } = require('mongo-seeding');
+const path = require('path');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 const jsonSchema = require('chai-json-schema');
 const app = require('../app.js');
 const should = chai.should();
 
+// schemas for validation
 const taskSchema = require('./schema/task');
 const todoSchema = require('./schema/todo');
 const todosSchema = require('./schema/todos');
 
+// create seeder object (initializing database before each test)
+const seeder = new Seeder({
+  database: mongoDb,
+  dropDatabase: true
+});
+
+// read collections for seed test database
+const collections = seeder.readCollectionsFromPath(path.resolve('./test/data/'));
+
+// use chai middleware
 chai.use(chaiHttp);
 chai.use(jsonSchema);
 
 describe('/todos', () => {
+
+  // drop current db and import startup data before each test
+  beforeEach(done => {
+    seeder.import(collections)
+      .then(() => done())
+      .catch(err => console.log(err));
+  });
+
   it('should GET all todo lists', done => {
     chai.request(app)
       .get('/api/todos')
@@ -19,7 +41,9 @@ describe('/todos', () => {
         should.not.exist(err);
         should.exist(res);
         res.should.have.status(200);
-        res.body.should.be.jsonSchema(todosSchema);
+        const body = res.body;
+        body.should.be.jsonSchema(todosSchema);
+        body.length.should.be.equal(3);
         done();
       });
   });
