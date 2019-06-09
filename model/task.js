873e708ec37b6560c6972d9c86c539b24dd78dae
Martin Šimeček <simecek.m@seznam.cz>
2019-06-09 19:11:24 +0200

    improve code quality - apply linting rules

--- model/task.js
+++ model/task.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-	text: {
-		type: String,
-		required: [true, 'Text describing this Task is required!']
-	},
-	completed: {
-		type: Boolean,
-		default: false,
-		required: [true, 'State of task is required!']
-	}
+  text: {
+    type: String,
+    required: [true, 'Text describing this Task is required!']
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+    required: [true, 'State of task is required!']
+  }
 });
 
 const Task = mongoose.model('task', TaskSchema);
