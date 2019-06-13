4e7f364a39a76b148da4b0b4fb33ec1f30683a84
Martin Šimeček <simecek.m@seznam.cz>
2019-06-13 22:05:31 +0200

    add error schema for test purpose

---
+++ test/schema/error.js
@@ -0,0 +1,27 @@
+const taskSchema = {
+  title: 'task schema v1',
+  type: 'object',
+  properties: {
+    message: {
+      type: 'string'
+    },
+    name: {
+      type: 'string'
+    },
+    stringValue: {
+      type: 'string'
+    },
+    kind: {
+      type: 'string'
+    },
+    value: {
+      type: 'string'
+    },
+    path: {
+      type: 'string'
+    }
+  },
+  required: ['message', 'name', 'stringValue', 'kind', 'value', 'path']
+};
+
+module.exports = taskSchema;
