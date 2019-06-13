c1f0d75211072ab07310b4d83c6385db740adf62
Martin Šimeček <simecek.m@seznam.cz>
2019-06-13 21:07:38 +0200

    API return new document after modification

--- router/routes/todo.js
+++ router/routes/todo.js
@@ -5,39 +5,36 @@ const Todo = require('~model/todo');
 const Task = require('~model/task');
 
 // get all todo lists
-router.get('/todos', function (req, res) {
+router.get('/todos', function (req, res, next) {
   Todo.find()
     .then(data => res.send(data))
-    .catch(error => {
-      console.log(error);
-      res.end();
-    });
+    .catch(next);
 });
 
 // create new todo list with default title ('Title') end empty tasks
-router.post('/todos', function (req, res) {
+router.post('/todos', function (req, res, next) {
   Todo.create({ title: 'Title' })
     .then(data => res.send(data))
-    .catch(error => console.log(error));
+    .catch(next);
 });
 
 // get specific todo list
 router.get('/todos/:id', function (req, res, next) {
-  Todo.findOne({ _id: req.params.id })
+  Todo.findById(req.params.id)
     .then(data => res.send(data))
     .catch(next);
 });
 
 // update todo list
 router.put('/todos/:id', function (req, res, next) {
-  Todo.updateOne({ _id: req.params.id }, req.body)
+  Todo.findByIdAndUpdate(req.params.id, req.body, { new: true })
     .then(data => res.send(data))
     .catch(next);
 });
 
 // delete todo list
 router.delete('/todos/:id', function (req, res, next) {
-  Todo.deleteOne({ _id: req.params.id })
+  Todo.findByIdAndDelete(req.params.id)
     .then(data => res.send(data))
     .catch(next);
 });
@@ -45,22 +42,22 @@ router.delete('/todos/:id', function (req, res, next) {
 // create task and add it to todo list
 router.post('/todos/:id/tasks', function (req, res, next) {
   let task = new Task(req.body);
-  Todo.updateOne({ _id: req.params.id }, { '$push': { tasks: task }}, { runValidators: true })
-    .then(data => res.send(data))
+  Todo.findByIdAndUpdate(req.params.id, { '$push': { tasks: task }}, { runValidators: true, new: true })
+    .then(() => res.send(task))
     .catch(next);
 });
 
 // delete specific task
 router.delete('/todos/:todoId/tasks/:taskId', function (req, res, next) {
-  Todo.updateOne({ _id: req.params.todoId }, { '$pull': { tasks: { _id: req.params.taskId }}})
-    .then(data => res.send(data))
+  Todo.findOneAndUpdate({ _id: req.params.todoId,  tasks: { '$elemMatch': { _id: req.params.taskId }}}, { '$pull': { tasks: { _id: req.params.taskId }}}, { new: true })
+    .then(data => res.send(data.tasks[0]))
     .catch(next);
 });
 
 // update specific task
 router.put('/todos/:todoId/tasks/:taskId', function (req, res, next) {
-  Todo.updateOne({ _id: req.params.todoId, tasks: { '$elemMatch': { _id: req.params.taskId }}}, { '$set': { 'tasks.$.completed': req.body.completed }}, { runValidators: true })
-    .then(data => res.send(data))
+  Todo.findOneAndUpdate({ _id: req.params.todoId,  tasks: { '$elemMatch': { _id: req.params.taskId }}}, { '$set': { 'tasks.$.completed': req.body.completed }}, { runValidators: true, new: true })
+    .then(data => res.send(data.tasks[0]))
     .catch(next);
 });
 
