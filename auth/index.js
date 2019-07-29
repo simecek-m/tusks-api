0b67da03c6bc6e5da247672ffb8424e00879c23b
Martin Šimeček <simecek.m@seznam.cz>
2019-07-29 20:43:53 +0200

    add error handler when missing jwt token

--- auth/index.js
+++ auth/index.js
@@ -5,11 +5,18 @@ const AUTHORIZATION_HEADER = 'authorization';
 const BASE64_ENCODING = 'base64';
 
 function decode (req, res, next) {  
-  const token = req.header(AUTHORIZATION_HEADER);
   logger.info('Verifying JWT authorization token.');
-  const payload = jwt.verify(token.slice(7), Buffer.from(googleJwtSignatureSecret, BASE64_ENCODING));
-  logger.info(`JWT token was successfully verified. Payload: ${JSON.stringify(payload)}`);
-  next();
+  const token = req.header(AUTHORIZATION_HEADER);
+  if (token) {
+    const payload = jwt.verify(token.slice(7), Buffer.from(googleJwtSignatureSecret, BASE64_ENCODING));
+    logger.info(`JWT token was successfully verified. Payload: ${JSON.stringify(payload)}`);
+    next();
+  } else {
+    next({
+      status: 401,
+      message: 'Authorization header (bearer token) is missing in the request!'
+    });
+  }
 }
 
 module.exports = decode;
