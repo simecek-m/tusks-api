ac9c34425bdbff72cfc90260bdba08b9cfe0fdbd
Martin Šimeček <simecek.m@seznam.cz>
2019-07-29 20:42:28 +0200

    improve error middleware: optional response status

--- middleware/error.js
+++ middleware/error.js
@@ -1,8 +1,8 @@
 const logger = require('~logger');
 
 function errorHandling (error, req, res, next) {
-  logger.error(`Error message: ${error.message}!`);
-  res.status(400).send(error);
+  logger.error(error.message);
+  res.status(error.status || 400).send(error);
   next();
 }
 
