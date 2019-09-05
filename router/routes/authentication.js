f83beae1f2a485e86be57457db792881570b77c2
Martin Šimeček <simecek.m@seznam.cz>
2019-09-05 21:19:15 +0200

    remove unused public routes

--- router/routes/authentication.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// get all todo lists
-router.post('/authentication', function (req, res) {
-  res.send({ message: 'Authentication is not implemented yet!' });
-});
-
-module.exports = router;
