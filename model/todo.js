b97462474421db1b5bb53e24e5475b579929d67f
Martin Šimeček <simecek.m@seznam.cz>
2019-10-05 22:49:49 +0200

    add author field into todo list model

--- model/todo.js
+++ model/todo.js
@@ -4,9 +4,13 @@ const Task = require('~model/task');
 const todoSchema = new mongoose.Schema({
   title: {
     type: String,
-    required: [true, 'Title field for todo list is required!']
+    required: [true, 'Title field of todo list is required!']
   },
-  tasks: [Task.schema]
+  tasks: [Task.schema],
+  author: {
+    type: String,
+    required: [true, 'Author field of todo list is required!']
+  }
 });
 
 const Todo = mongoose.model('todo', todoSchema);
