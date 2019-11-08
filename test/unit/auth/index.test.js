91f8ccd613cbbe39f1c5427ba2919d98d6d8b505
Martin Šimeček <simecek.m@seznam.cz>
2019-11-08 16:09:08 +0100

    add test for auth middleware with authorization header with undecodable token

--- test/unit/auth/index.test.js
+++ test/unit/auth/index.test.js
@@ -2,7 +2,7 @@ const sinon = require('sinon');
 const auth = require('~auth');
 const middleware = require('~mocks/middleware');
 
-describe('auth middleware - check request header', () => {
+describe('auth middleware - check authorization header', () => {
 
   it('request without authorization header', () => {
     const authenticateSpy = sinon.spy(auth, 'authenticate');
@@ -25,6 +25,21 @@ describe('auth middleware - check request header', () => {
     mockRequest.expects('header').once().withArgs('authorization').returns('TOKEN');
     const mockMiddleware = sinon.mock(middleware);
     mockMiddleware.expects('next').once().withArgs({ message: auth.MESSAGE_WRONG_FORMAT, status: auth.HTTP_STATUS_FORBIDDEN });
+    auth.authenticate(req, {}, middleware.next);
+    authenticateSpy.calledOnce.should.be.true;
+    mockRequest.verify();
+    mockMiddleware.verify();
+    authenticateSpy.restore();
+  });
+
+  it('request with authorization header with undecodable token', () => {
+    const TOKEN = 'Bearer TOKEN';
+    const authenticateSpy = sinon.spy(auth, 'authenticate');
+    const req = middleware.request;
+    const mockRequest = sinon.mock(req);
+    mockRequest.expects('header').once().withArgs('authorization').returns(TOKEN);
+    const mockMiddleware = sinon.mock(middleware);
+    mockMiddleware.expects('next').once().withArgs({ message: auth.MESSAGE_JWT_NOT_DECODED, status: auth.HTTP_STATUS_UNAUTHORIZED });
     auth.authenticate(req, {}, middleware.next);
     authenticateSpy.calledOnce.should.be.true;
     mockRequest.verify();
