600ce49e1e895bcc7e396044307d8299adec9d20
Martin Šimeček <simecek.m@seznam.cz>
2019-11-08 15:29:40 +0100

    create error message constants

--- src/auth/index.js
+++ src/auth/index.js
@@ -2,10 +2,18 @@ const logger = require('~logger');
 const jwt = require('jsonwebtoken');
 const { getGooglePublicKeys } = require('~auth/google');
 
-const HTTP_STATUS_UNAUTHORIZED = 401;
-const HTTP_STATUS_FORBIDDEN = 403;
 const AUTHORIZATION_HEADER = 'authorization';
 const GOOGLE_ISSUER = 'accounts.google.com';
+
+const HTTP_STATUS_FORBIDDEN = 403;
+const HTTP_STATUS_UNAUTHORIZED = 401;
+
+const MESSAGE_NO_PAYLOAD = 'JWT has no payload!';
+const MESSAGE_WRONG_ISSUER = `Issuer of the JWT is not ${GOOGLE_ISSUER}. Verification denied!`;
+const MESSAGE_JWT_NOT_DECODED = 'Authorization token could not be decoded!';
+const MESSAGE_GOOGLE_KEY_NOT_FOUND = 'No Google public key was found for verify signature! Verification denied!';
+const MESSAGE_NO_EMAIL_FIELD = 'JWT payload has no email field.';
+const MESSAGE_WRONG_FORMAT = 'Authorization header is missing or has wrong format. Required \'Authorization\': \'Bearer {TOKEN}\'!';
 
 function authenticate (req, res, next) {
   logger.info('Verifying JWT authorization token.');
@@ -17,23 +25,23 @@ function authenticate (req, res, next) {
     if (!tokenDecoded) {
       next({
         status: HTTP_STATUS_UNAUTHORIZED,
-        message: `Authorization token: ${token} could not be decoded!`
+        message: MESSAGE_JWT_NOT_DECODED
       });
     }
     else if (!tokenDecoded.payload) {
       next({
         status: HTTP_STATUS_UNAUTHORIZED,
-        message: 'JWT has no payload!'
+        message: MESSAGE_NO_PAYLOAD
       });
     } else if (tokenDecoded.payload.iss != GOOGLE_ISSUER) {
       next({
         status: HTTP_STATUS_UNAUTHORIZED,
-        message: `Issuer of the JWT is not ${GOOGLE_ISSUER}. Verification denied!`
+        message: MESSAGE_WRONG_ISSUER
       });
     } else if (!googlePublicKeys.hasOwnProperty(tokenDecoded.header.kid)) {
       next({
         status: HTTP_STATUS_UNAUTHORIZED,
-        message: 'No Google public key for verify signature! Verification denied!'
+        message: MESSAGE_GOOGLE_KEY_NOT_FOUND
       });
     } else {
       const payload = jwt.verify(
@@ -48,14 +56,14 @@ function authenticate (req, res, next) {
       } else {
         next({
           status: HTTP_STATUS_UNAUTHORIZED,
-          message: 'JWT payload has no email field.'
+          message: MESSAGE_NO_EMAIL_FIELD
         });
       }
     }
   } else {
     next({
       status: HTTP_STATUS_FORBIDDEN,
-      message: 'Authorization header is missing or has wrong format. Required \'Authorization:\': \'Bearer {TOKEN}\'!'
+      message: MESSAGE_WRONG_FORMAT
     });
   }
 }
@@ -65,5 +73,11 @@ module.exports = {
   HTTP_STATUS_UNAUTHORIZED,
   HTTP_STATUS_FORBIDDEN,
   AUTHORIZATION_HEADER,
-  GOOGLE_ISSUER
+  GOOGLE_ISSUER,
+  MESSAGE_NO_PAYLOAD,
+  MESSAGE_WRONG_ISSUER,
+  MESSAGE_JWT_NOT_DECODED,
+  MESSAGE_GOOGLE_KEY_NOT_FOUND,
+  MESSAGE_NO_EMAIL_FIELD,
+  MESSAGE_WRONG_FORMAT
 };
