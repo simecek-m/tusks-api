323cc4c1d756aeaab4f6f5f7bca7595b3215fcc9
Martin Šimeček <simecek.m@seznam.cz>
2019-11-13 08:04:20 +0100

    add test for create main logger

---
+++ test/unit/logger/createLogger.test.js
@@ -0,0 +1,14 @@
+const sinon = require('sinon');
+const { loggers } = require('winston');
+const createLogger = require('~logger/createLogger');
+const { MAIN_LOGGER } = require('~constants');
+
+describe('logger - create', () => {
+  it('create main logger', done => {
+    const addLoggerSpy = sinon.spy(loggers, 'add');
+    createLogger();
+    addLoggerSpy.callCount.should.be.equals(1);
+    addLoggerSpy.firstCall.args[0].should.be.equals(MAIN_LOGGER);
+    done();
+  });
+});
