4f013d7ae03c67340ca0c21a3e3753b2316b4771
Martin Šimeček <simecek.m@seznam.cz>
2019-11-14 09:32:15 +0100

    add test for task model

---
+++ test/unit/model/task.test.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const sinon = require('sinon');
+const chai = require('chai');
+
+chai.should();
+
+const TASK_FIELDS = ['text', 'completed'];
+
+describe('task model', () => {
+  it('create schema for task model', done => {
+    const mongooseSchemaSpy = sinon.spy(mongoose, 'Schema');
+    require('~model/task');
+    const firstArgument = mongooseSchemaSpy.firstCall.args[0];
+    firstArgument.should.be.an('object');
+    Object.keys(firstArgument).should.be.eql(TASK_FIELDS);
+    sinon.restore();
+    done();
+  });
+});
