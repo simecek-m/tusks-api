db84a0f1eb87ba5443a90dd429db874df01b29d1
Martin Šimeček <simecek.m@seznam.cz>
2019-11-18 15:41:30 +0100

    add schemas for testing purposes

---
+++ test/data/schema/todo.js
@@ -0,0 +1,24 @@
+const taskSchema = require('~schema/task');
+
+const todoSchema = {
+  title: 'todo schema v1',
+  type: 'object',
+  properties: {
+    _id: {
+      type: 'string',
+    },
+    title: {
+      type: 'string'
+    },
+    tasks: {
+      type: 'array',
+      items: taskSchema
+    },
+    author: {
+      type: 'string'
+    }
+  },
+  required: ['_id', 'title', 'tasks']
+};
+
+module.exports = todoSchema;
