93a62759ca72231c6fd4cde83c5eaacc140579a7
Martin Šimeček <simecek.m@seznam.cz>
2019-11-19 13:33:42 +0100

    delte require cache for auth/google module

--- test/unit/auth/google.test.js
+++ test/unit/auth/google.test.js
@@ -1,7 +1,8 @@
 const sinon = require('sinon');
 const axios = require('axios');
 const chai = require('chai');
-const google = require('~auth/google');
+const { deleteModuleCache } = require('~test-helper/index');
+
 const { GOOGLE_API_KEYS_URL } = require('~config');
 
 const should = chai.should();
@@ -13,14 +14,18 @@ const GOOGLE_KEYS_TEST = {
 };
 
 describe('auth middleware - fetch google key for RSA', () => {
+  
   it('load google public keys for verify JWT signature', async () => {
+    deleteModuleCache('~auth/google');
+    const googleApi = require('~auth/google');
     const axiosMock = sinon.mock(axios);
     axiosMock.expects('get').once().withArgs(GOOGLE_API_KEYS_URL).returns(GOOGLE_KEYS_TEST);
-    let result = google.getGooglePublicKeys();
+    let result = googleApi.getGooglePublicKeys();
     should.not.exist(result);
-    await google.fetchGooglePublickKeys();
-    result = google.getGooglePublicKeys();
+    await googleApi.fetchGooglePublickKeys();
+    result = googleApi.getGooglePublicKeys();
     result.should.be.eql(GOOGLE_KEYS_TEST.data);
     axiosMock.verify();
+    axiosMock.restore();
   });
 });
