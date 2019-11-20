177075774e1fb52c818c45ea74c497a68a402663
Martin Šimeček <simecek.m@seznam.cz>
2019-11-20 09:14:46 +0100

    add integration test for API - GET /api/todos/:id endpoint

--- test/integration/api.test.js
+++ test/integration/api.test.js
@@ -20,7 +20,10 @@ const should = chai.should();
 const TEST_EMAIL = 'todo@todo.com';
 const TEST_TODO_LIST_TITLE = 'test';
 
-describe('/todos', () => {
+const TEST_TODO_LIST_ID = '5cfe9d771b6ff31cc8e31fb3';
+const TEST_TODO_TITLE = 'Work';
+
+describe('API endpoints', () => {
 
   // run application before tests begin & turn off authentication
   before(async () => {
@@ -79,6 +82,17 @@ describe('/todos', () => {
     response.body.title.should.be.an('string').that.is.equal(TEST_TODO_LIST_TITLE);
     response.body.tasks.should.be.an('array').that.is.empty;
     response.body.__v.should.be.an('number').that.is.equal(0);
+  });
+
+  it('should GET specific todo list', async () => {
+    const response = await chai.request(this.server).get(`/api/todos/${TEST_TODO_LIST_ID}`);
+    should.exist(response);
+    response.should.have.status(200);
+    response.body.should.have.jsonSchema(todoSchema);
+    response.body._id.should.be.an('string').that.is.equal(TEST_TODO_LIST_ID);
+    response.body.title.should.be.an('string').that.is.equal(TEST_TODO_TITLE);
+    response.body.tasks.should.be.an('array').that.have.lengthOf(2);
+    response.body.author.should.be.an('string').that.is.equals(TEST_EMAIL);
   });
 
 });
