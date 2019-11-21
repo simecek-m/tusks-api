4e91436ca3be6436acb1e88e5017e55658f6b81f
Martin Šimeček <simecek.m@seznam.cz>
2019-11-21 09:29:55 +0100

    fix todo and task mongoose model tests

--- test/unit/model/task.test.js
+++ test/unit/model/task.test.js
@@ -1,20 +1,28 @@
 const mongoose = require('mongoose');
 const sinon = require('sinon');
 const chai = require('chai');
+const { deleteModuleCache, deleteMongooseModel } = require('~test-helper');
 
 chai.should();
 
 const TASK_FIELDS = ['text', 'completed'];
 
-//TODO: fix Schema wrapper
-describe.skip('task model', () => {
+describe('task model', () => {
   it('create schema for task model', done => {
+    deleteMongooseModel('task');
+    deleteModuleCache('~model/task');
     const mongooseSchemaSpy = sinon.spy(mongoose, 'Schema');
+    const mongooseModelSpy = sinon.spy(mongoose, 'model');
     require('~model/task');
+    mongooseSchemaSpy.callCount.should.be.equals(1);
     const firstArgument = mongooseSchemaSpy.firstCall.args[0];
     firstArgument.should.be.an('object');
     Object.keys(firstArgument).should.be.eql(TASK_FIELDS);
-    sinon.restore();
+    mongooseModelSpy.callCount.should.be.equals(1);
+    mongooseModelSpy.firstCall.args[0].should.be.an('string').that.is.equals('task');
+    mongooseModelSpy.firstCall.args[1].should.be.an('object');
+    mongooseSchemaSpy.restore();
+    mongooseModelSpy.restore();
     done();
   });
 });
