5e86eeca24a12e075f7d8c53ac2c4c4220f75e9c
Martin Šimeček <simecek.m@seznam.cz>
2022-05-28 20:19:23 +0200

    update all dependencies

--- src/database/index.js
+++ src/database/index.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 const logger = require('~logger');
 
-const OPTIONS = { useCreateIndex:true, useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true };
-
 async function connect () {
   logger.info('Opening mongoose connection.');
-  return mongoose.connect(process.env.MONGO_URL, OPTIONS);
+  return mongoose.connect(process.env.MONGO_URL);
 }
 
 async function disconnect () {
@@ -16,5 +14,4 @@ async function disconnect () {
 module.exports = {
   connect,
   disconnect,
-  OPTIONS
 };
