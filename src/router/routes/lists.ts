8a3fcd972e895fd29a3a34dc8a08e60fe26cee9f
Martin Šimeček <simecek.m@seznam.cz>
2022-09-14 22:23:33 +0200

    rewrite todo list routes to use async await

--- src/router/routes/lists.ts
+++ src/router/routes/lists.ts
@@ -9,68 +9,85 @@ import { validate } from "middleware/validation/validate";
 const router = Router();
 
 // get all todo lists
-router.get("/lists", function (req, res, next) {
-  List.find({ author: req.auth.payload.sub })
-    .populate("tags")
-    .then((data) => res.send(data))
-    .catch((e) => next(new UnexpectedError(e)));
+router.get("/lists", async (req, res, next) => {
+  try {
+    const result = await List.find({ author: req.auth.payload.sub }).populate(
+      "tags"
+    );
+    res.send(result);
+  } catch (e) {
+    next(new UnexpectedError(e));
+  }
 });
 
 // create new todo list
-router.post("/lists", validate(newListSchema), function (req, res, next) {
-  List.create({
-    ...req.body,
-    author: req.auth.payload.sub,
-  })
-    .then((data) => res.send(data))
-    .catch((e) => next(new UnexpectedError(e)));
+router.post("/lists", validate(newListSchema), async (req, res, next) => {
+  try {
+    const result = await List.create({
+      ...req.body,
+      author: req.auth.payload.sub,
+    });
+    res.send(result);
+  } catch (e) {
+    next(new UnexpectedError(e));
+  }
 });
 
 // get specific todo list
-router.get("/lists/:id", function (req, res, next) {
-  List.findOne({ author: req.auth.payload.sub, _id: req.params.id })
-    .populate("tags")
-    .then((data) => {
-      if (data) {
-        res.send(data);
-      } else {
-        next(new HttpError(404, `Todo list (${req.params.id}) was not found!`));
-      }
-    })
-    .catch((e) => next(new UnexpectedError(e)));
+router.get("/lists/:id", async (req, res, next) => {
+  try {
+    const result = await List.findOne({
+      author: req.auth.payload.sub,
+      _id: req.params.id,
+    }).populate("tags");
+
+    if (result) {
+      res.send(result);
+    } else {
+      next(new HttpError(404, `Todo list (${req.params.id}) was not found!`));
+    }
+  } catch (e) {
+    next(new UnexpectedError(e));
+  }
 });
 
 // update todo list
-router.put("/lists/:id", validate(udateListSchema), function (req, res, next) {
-  List.findOneAndUpdate(
-    { author: req.auth.payload.sub, _id: req.params.id },
-    req.body,
-    {
-      new: true,
-      runValidators: true,
-    }
-  )
-    .then((data) => {
-      if (data) {
-        res.send(data);
-      } else {
-        next(new HttpError(404, `Todo list (${req.params.id}) was not found!`));
+router.put("/lists/:id", validate(udateListSchema), async (req, res, next) => {
+  try {
+    const result = await List.findOneAndUpdate(
+      { author: req.auth.payload.sub, _id: req.params.id },
+      req.body,
+      {
+        new: true,
+        runValidators: true,
       }
-    })
-    .catch((e) => next(new UnexpectedError(e)));
+    );
+    if (result) {
+      res.send(result);
+    } else {
+      next(new HttpError(404, `Todo list (${req.params.id}) was not found!`));
+    }
+  } catch (e) {
+    next(new UnexpectedError(e));
+  }
 });
 
 // delete todo list
-router.delete("/lists/:id", function (req, res, next) {
-  List.findOneAndDelete({ author: req.auth.payload.sub, _id: req.params.id })
-    .then((data) => {
-      if (data) {
-        res.send(data);
-      } else {
-        next(new HttpError(404, `Todo list (${req.params.id}) was not found!`));
-      }
-    })
-    .catch((e) => next(new UnexpectedError(e)));
+router.delete("/lists/:id", async (req, res, next) => {
+  try {
+    const result = await List.findOneAndDelete({
+      author: req.auth.payload.sub,
+      _id: req.params.id,
+    });
+
+    if (result) {
+      res.send(result);
+    } else {
+      next(new HttpError(404, `Todo list (${req.params.id}) was not found!`));
+    }
+  } catch (e) {
+    next(new UnexpectedError(e));
+  }
 });
 
 export default router;
