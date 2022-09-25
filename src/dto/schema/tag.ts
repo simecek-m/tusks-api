c9723e5fdb5f20f4ac275dfb681132f84526b5eb
Martin Šimeček <simecek.m@seznam.cz>
2022-09-25 21:26:10 +0200

    add error messages to Tag DTO validation

--- src/dto/schema/tag.ts
+++ src/dto/schema/tag.ts
@@ -14,15 +14,15 @@ const tagSchema = object({
         "Light version of the color field must be in HEX format (#FFFFFF)"
       )
       .uppercase()
-      .required(),
+      .required("Light version of the color field is required!"),
     dark: string()
       .matches(
         COLOR_HEX_FORMAT,
         "Dark version of the color field must be in HEX format (#FFFFFF)"
       )
       .uppercase()
-      .required(),
-  }).required(),
+      .required("Dark version of the color field is required!"),
+  }),
 });
 
 export default tagSchema;
