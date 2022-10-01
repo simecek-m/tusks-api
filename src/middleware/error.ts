085198ad0d3bef54a0357231a4ea01ead7270e1e
Martin Šimeček <simecek.m@seznam.cz>
2022-10-01 11:16:14 +0200

    normalize error responses for 401 and 500 status codes

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -2,10 +2,17 @@ import { HttpError } from "error/HttpError";
 import { UnexpectedError } from "error/UnexpectedError";
 import { ValidationError } from "error/ValidationError";
 import { NextFunction, Request, Response } from "express";
+import { UnauthorizedError } from "express-oauth2-jwt-bearer";
 import logger from "logger";
 
+interface ErrorResponse {
+  code: number;
+  message: string;
+  description: string;
+}
+
 export function errorHandler(
-  error: HttpError | ValidationError | UnexpectedError,
+  error: HttpError | ValidationError | UnexpectedError | UnauthorizedError,
   req: Request,
   res: Response,
   next: NextFunction
@@ -15,7 +22,20 @@ export function errorHandler(
     res.status(400).send(error);
   } else if (error instanceof UnexpectedError) {
     logger.error(`${error.message} -> ${req.path}:`, error.cause);
-    res.status(error.status).send(error);
+    const response: ErrorResponse = {
+      code: error.status,
+      message: error.message,
+      description: error.cause.message,
+    };
+    res.status(error.status).send(response);
+  } else if (error instanceof UnauthorizedError) {
+    logger.error(`Unathorized error -> ${error.headers["WWW-Authenticate"]}`);
+    const response: ErrorResponse = {
+      code: error.status,
+      message: error.message,
+      description: error.headers["WWW-Authenticate"],
+    };
+    res.status(error.status).send(response);
   } else {
     logger.error(error.message);
     res.status(error.status || 400).send(error);
