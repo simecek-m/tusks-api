2a1234ad836b8289fbc755087ec52e4e43d98db0
Martin Šimeček <simecek.m@seznam.cz>
2022-10-04 22:46:08 +0200

    implement PUT /teams/:id endpoint

--- src/router/routes/teams.ts
+++ src/router/routes/teams.ts
@@ -1,5 +1,6 @@
 import { ROUTE_TEAMS } from "constant";
 import Team from "database/model/Team";
+import { HttpError } from "error/HttpError";
 import { UnexpectedError } from "error/UnexpectedError";
 import { Router } from "express";
 import { IMember } from "types";
@@ -27,6 +28,27 @@ router.post(`/${ROUTE_TEAMS}`, async function (req, res, next) {
     };
     const result = await Team.create({ ...req.body, members: [member] });
     res.send(result);
+  } catch (e) {
+    next(new UnexpectedError(e));
+  }
+});
+
+// update team
+router.put(`/${ROUTE_TEAMS}/:id`, async function (req, res, next) {
+  try {
+    const result = await Team.findOneAndUpdate(
+      {
+        _id: req.params.id,
+        members: { $elemMatch: { user: req.auth.payload.sub } },
+      },
+      req.body,
+      { runValidators: true, new: true }
+    );
+    if (result) {
+      res.send(result);
+    } else {
+      next(new HttpError(404, `Team (${req.params.id}) was not found!`));
+    }
   } catch (e) {
     next(new UnexpectedError(e));
   }
