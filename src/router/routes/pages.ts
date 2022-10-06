eae779854a540b5bda02afbc9cba2dbaab7b8689
Martin Šimeček <simecek.m@seznam.cz>
2022-10-06 21:41:53 +0200

    implement GET /pages/:id endpoint

--- src/router/routes/pages.ts
+++ src/router/routes/pages.ts
@@ -4,6 +4,7 @@ import Project from "database/model/Project";
 import { HttpError } from "error/HttpError";
 import { UnexpectedError } from "error/UnexpectedError";
 import { Router } from "express";
+import { IProject } from "types";
 
 const router = Router();
 
@@ -20,6 +21,25 @@ router.post(`/${ROUTE_PAGES}`, async (req, res, next) => {
       res.send(result.pages.find((page) => page.id === newPage.id));
     } else {
       next(new HttpError(404, `Project (${projectId}) was not found!`));
+    }
+  } catch (e) {
+    next(new UnexpectedError(e));
+  }
+});
+
+router.get(`/${ROUTE_PAGES}/:id`, async (req, res, next) => {
+  try {
+    const result: IProject = await Project.findOne({
+      author: req.auth.payload.sub,
+      pages: { $elemMatch: { _id: req.params.id } },
+    });
+    if (result) {
+      console.log(result);
+      res.send(
+        result.pages.find((page) => page.id.toString() === req.params.id)
+      );
+    } else {
+      next(new HttpError(404, `Page (${req.params.id}) was not found!`));
     }
   } catch (e) {
     next(new UnexpectedError(e));
