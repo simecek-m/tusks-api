63fa2e7c21fc2fc9e339b4c18ce004cc53559a9c
Martin Šimeček <simecek.m@seznam.cz>
2023-01-09 18:56:19 +0100

    add page dto schema

---
+++ src/dto/schema/page.ts
@@ -0,0 +1,33 @@
+import { DEFAULT_XSS_OPTIONS } from "constant";
+import { AVAILABLE_ICONS } from "types/icon";
+import xss from "xss";
+import { array, object, string, mixed } from "yup";
+import tagSchema from "./tag";
+import taskSchema from "./task";
+
+const ALL_ICONS = [...AVAILABLE_ICONS];
+
+const basePageSchema = object({
+  name: string()
+    .transform((data) => xss(data, DEFAULT_XSS_OPTIONS))
+    .trim()
+    .required(),
+  icon: string().oneOf(ALL_ICONS).trim().required(),
+  description: string()
+    .transform((data) => xss(data, DEFAULT_XSS_OPTIONS))
+    .trim(),
+  tags: array().of(tagSchema),
+});
+
+const listPageSchema = basePageSchema.shape({
+  tasks: array().of(taskSchema),
+});
+
+// TODO: validate markdown format of content
+const markdownPageSchema = basePageSchema.shape({
+  content: string().required(),
+});
+
+const pageSchema = mixed().oneOf([listPageSchema, markdownPageSchema]);
+
+export default pageSchema;
