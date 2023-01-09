77584bf0735c0769a2242b764b9f2880a5be7020
Martin Šimeček <simecek.m@seznam.cz>
2023-01-09 19:00:43 +0100

    add project dto schema

---
+++ src/dto/schema/project.ts
@@ -0,0 +1,29 @@
+import { DEFAULT_XSS_OPTIONS } from "constant";
+import { AVAILABLE_ICONS } from "types/icon";
+import xss from "xss";
+import { array, object, string } from "yup";
+import colorSchema from "./color";
+import pageSchema from "./page";
+import shareSchema from "./share";
+import tagSchema from "./tag";
+
+const ALL_ICONS = [...AVAILABLE_ICONS];
+
+const projectSchema = object({
+  name: string()
+    .transform((data) => xss(data, DEFAULT_XSS_OPTIONS))
+    .trim()
+    .required(),
+  icon: string().oneOf(ALL_ICONS).trim().required(),
+  description: string()
+    .transform((data) => xss(data, DEFAULT_XSS_OPTIONS))
+    .trim()
+    .nullable(),
+  tags: array().of(tagSchema).required(),
+  color: colorSchema.required(),
+  pages: array().of(pageSchema).required(),
+  defaultPage: pageSchema.nullable(),
+  share: shareSchema.required(),
+});
+
+export default projectSchema;
