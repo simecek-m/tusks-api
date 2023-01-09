5aaba3de62046a037be154f3fc8f15ec4c20f4a9
Martin Šimeček <simecek.m@seznam.cz>
2023-01-09 19:26:15 +0100

    validate POST /projects endpoint DTO

--- src/router/routes/projects.ts
+++ src/router/routes/projects.ts
@@ -1,8 +1,10 @@
 import { ROUTE_PROJECTS } from "constant";
 import Project from "database/model/Project";
+import projectSchema from "dto/schema/project";
 import { HttpError } from "error/HttpError";
 import { UnexpectedError } from "error/UnexpectedError";
 import { Router } from "express";
+import { validate } from "middleware/validation/validate";
 
 const router = Router();
 
@@ -19,17 +21,21 @@ router.get(`/${ROUTE_PROJECTS}`, async (req, res, next) => {
 });
 
 // create new project
-router.post(`/${ROUTE_PROJECTS}`, async (req, res, next) => {
-  try {
-    const result = await Project.create({
-      ...req.body,
-      owner: req.auth.payload.sub,
-    });
-    res.send(result);
-  } catch (e) {
-    next(new UnexpectedError(e));
+router.post(
+  `/${ROUTE_PROJECTS}`,
+  validate(projectSchema),
+  async (req, res, next) => {
+    try {
+      const result = await Project.create({
+        ...req.body,
+        owner: req.auth.payload.sub,
+      });
+      res.send(result);
+    } catch (e) {
+      next(new UnexpectedError(e));
+    }
   }
-});
+);
 
 // retrieve one specific project by id
 router.get(`/${ROUTE_PROJECTS}/:id`, async (req, res, next) => {
