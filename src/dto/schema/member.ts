398a534c7a404b4fa42275297d7eb0d03720c167
Martin Šimeček <simecek.m@seznam.cz>
2023-01-10 21:45:23 +0100

    fix: member dto scheme use string ID of user

--- src/dto/schema/member.ts
+++ src/dto/schema/member.ts
@@ -1,9 +1,8 @@
 import { AVAILABLE_MEMEBR_ROLES } from "constant";
 import { object, string } from "yup";
-import userSchema from "./user";
 
 const memberSchema = object({
-  user: userSchema.required(),
+  user: string().required(),
   role: string()
     .oneOf([...AVAILABLE_MEMEBR_ROLES])
     .required(),
